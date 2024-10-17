54b76635e8498fc562626a52c2149fa2fb8ef9c3
Mohamed Rageh <mhmdrgh@gmail.com>
2024-10-17 15:58:20 +0300

    run black

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
