e66166c8ef036746f705b22bdf81cdbdc410908b
Mohamed Rageh <mhmdrgh@gmail.com>
2024-10-17 15:55:37 +0300

    run black before push

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', None)
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
